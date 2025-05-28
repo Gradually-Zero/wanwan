7a893d6a2b546a7798ea598e2080ce3f72f3f85a
mnz8 <mnz8gg@gmail.com>
2025-05-29 01:27:56 +0800

    feat: 清空

--- entrypoints/background.ts
+++
@@ -1,6 +0,0 @@
-import { browser } from 'wxt/browser';
-import { defineBackground } from 'wxt/utils/define-background';
-
-export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
-});
